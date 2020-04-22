7f06130b16e8d4af58817987710d6d977c8cb024
meghamoree <meghamoree@gmail.com>
2020-04-22 17:56:08 +0530

    Add.Adding use case 2 to the branch resetBoard

--- TicTacToe1.sh
+++ TicTacToe1.sh
@@ -5,11 +5,26 @@ COLUMNNUMBER=3
 TOTALCOUNT=9
 
 function reset() {
- for (( row=0; row < $ROWNUMBER; row++ ))
- do
- for (( col=0; col < $COLUMNNUMBER; col++ ))
- do
- board[$row,$col]=" "
- done
- done
+ 	for (( row=0; row < $ROWNUMBER; row++ ))
+ 	do
+ 	for (( col=0; col < $COLUMNNUMBER; col++ ))
+ 	do
+ 	board[$row,$col]=" "
+ 	done
+ 	done
+}
+
+randomCheck=$((RANDOM%2))
+	function assignLetter() {
+	if (( $randomCheck == 0 ))
+ 	then
+ 		player="X"
+ 		computer="O"
+ 	else
+		player="O"
+ 		computer="X"
+ 	fi
+ 	echo "Player has been assigned $player"
+ 	echo "Computer has been assigned $computer"
+
 }
