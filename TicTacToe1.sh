f9aeb476236a57802c5015a87211615fc256137d
meghamoree <meghamoree@gmail.com>
2020-04-22 14:16:11 +0530

    Add.Adding Use Case1 to the branch resetBoard

--- TicTacToe1.sh
+++ TicTacToe1.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -x
 
+ROWNUMBER=3
+COLUMNNUMBER=3
+TOTALCOUNT=9
+
+function reset() {
+ for (( row=0; row < $ROWNUMBER; row++ ))
+ do
+ for (( col=0; col < $COLUMNNUMBER; col++ ))
+ do
+ board[$row,$col]=" "
+ done
+ done
+}
